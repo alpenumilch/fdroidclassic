82f4522c5204b01bdf6ad92011e5b4502d6f92e1
Marcus Hoffmann <bubu@bubu1.eu>
2022-05-28 18:07:48 +0200

    UpdatingRecevier: don't crash on null action
    * reformat

--- app/src/main/java/org/fdroid/fdroid/receiver/UpdatingReceiver.java
+++ app/src/main/java/org/fdroid/fdroid/receiver/UpdatingReceiver.java
@@ -13,24 +13,28 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class UpdatingReceiver extends BroadcastReceiver {
     SwipeRefreshLayout pullToRefresh;
+
     public UpdatingReceiver(SwipeRefreshLayout pullToRefresh) {
         this.pullToRefresh = pullToRefresh;
     }
 
     @Override
-        public void onReceive(Context context, Intent intent) {
-            String s = intent.getAction();
-            String action = intent.getAction();
-            if (TextUtils.isEmpty(action) || !action.equals(LOCAL_ACTION_STATUS)) {
-                return;
-            }
-
-            int resultCode = intent.getIntExtra(EXTRA_STATUS_CODE, -1);
-            if (resultCode == STATUS_INFO) {
-                pullToRefresh.setRefreshing(true);
-            }
-            // all other status codes are success/error, so we are finished then.
-            if (resultCode < STATUS_INFO)
-                pullToRefresh.setRefreshing(false);
+    public void onReceive(Context context, Intent intent) {
+        String s = intent.getAction();
+        String action = intent.getAction();
+        if (action == null) {
+            return;
         }
+        if (TextUtils.isEmpty(action) || !action.equals(LOCAL_ACTION_STATUS)) {
+            return;
+        }
+
+        int resultCode = intent.getIntExtra(EXTRA_STATUS_CODE, -1);
+        if (resultCode == STATUS_INFO) {
+            pullToRefresh.setRefreshing(true);
+        }
+        // all other status codes are success/error, so we are finished then.
+        if (resultCode < STATUS_INFO)
+            pullToRefresh.setRefreshing(false);
+    }
 }
